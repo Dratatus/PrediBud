20d900ad8d8e3828975a388ee6b01536a4b53b7a
Daniel <daniel.partatus@onet.pl>
2024-10-16 21:46:16 +0200

    Deleted Backend

--- Backend.Data/Models/Suppliers/Supplier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Data.Models.Suppliers
-{
-    public class Supplier
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public List<Material> AvailableMaterials { get; set; } = new List<Material>();
-    }
-}
