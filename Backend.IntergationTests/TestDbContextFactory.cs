d9b3a9ef7bf1000297bd96d60e3feb135484752c
Daniel <daniel.partatus@onet.pl>
2024-10-18 11:11:02 +0200

    added integration test, fixed Register body model, all tests working

---
+++ Backend.IntergationTests/TestDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Data.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backend.IntergationTests
+{
+    public class TestDbContextFactory : WebApplicationFactory<Backend.Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<PrediBudDBContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<PrediBudDBContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+            });
+        }
+    }
+}
